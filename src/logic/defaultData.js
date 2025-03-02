a890a4391355c80dd34a3c6ea195732411ff5b91
Anuj <122350668+anujbalak@users.noreply.github.com>
2025-03-02 07:59:35 +0530

    Create default deck data

---
+++ src/logic/defaultData.js
@@ -0,0 +1,59 @@
+let defaultData = {}
+export default defaultData = {
+    success: false,
+    deck_id: "id",
+    cards: [
+        {
+            code: "1",
+            image: "/back.png",
+            value: "1",
+            suit: "suit"
+        },
+        {
+            code: "2",
+            image: "/back.png",
+            value: "2",
+            suit: "suit"
+        },{
+            code: "3",
+            image: "/back.png",
+            value: "3",
+            suit: "suit"
+        },{
+            code: "4",
+            image: "/back.png",
+            value: "4",
+            suit: "suit"
+        },{
+            code: "5",
+            image: "/back.png",
+            value: "5",
+            suit: "suit"
+        },{
+            code: "6",
+            image: "/back.png",
+            value: "6",
+            suit: "suit"
+        },{
+            code: "7",
+            image: "/back.png",
+            value: "7",
+            suit: "suit"
+        },{
+            code: "8",
+            image: "/back.png",
+            value: "8",
+            suit: "suit"
+        },{
+            code: "9",
+            image: "/back.png",
+            value: "9",
+            suit: "suit"
+        },{
+            code: "0",
+            image: "/back.png",
+            value: "0",
+            suit: "suit"
+        },
+    ]
+}
