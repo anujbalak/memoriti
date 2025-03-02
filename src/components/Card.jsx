e2c22d9aa186a77cfa61ac78214dc6c4d5649d21
Anuj <122350668+anujbalak@users.noreply.github.com>
2025-03-02 07:59:03 +0530

    Flip animation with mutiple changes and fixes

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,33 @@
 
+export default function Card({
+    card, 
+    value, 
+    suit, 
+    code, 
+    cardClickHandler,
+    success
+    }) {
 
-export default function Card({card, value, suit, code, cardClickHandler}) {
     const name = `${value} (${suit.toLowerCase()})`
+    let face = 'back';
+    if (success) {
+        face = 'front';
+    }
     return (
-        <div className="card" onClick={cardClickHandler}>
-            <img src={card} alt={name} id={code}/>
-            <span className="card-name-container">
-                <p className="card-name">
-                    {name}
-                </p>
-            </span>
+        <div className="card-container" >
+            <div className={`card ${face}`} onClick={cardClickHandler}>
+                <img src={card} alt={name} id={code}/>
+                {success && 
+                    <span className="card-name-container">
+                        <p className="card-name">
+                            {name}
+                        </p>
+                    </span>
+                }
+                
+            </div>
         </div>
     )
 }
+
+
--- src/web/Board.jsx
+++ src/web/Board.jsx
@@ -1,17 +1,20 @@
 /* eslint-disable react/prop-types */
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState, useRef, use } from "react";
 import Header from "../components/Header";
 import Instructions from "../components/instuctions";
 import Card from "../components/Card.jsx";
 import { shuffleCards } from "../logic/Shuffle.js";
 import Button from "../components/Button.jsx";
+import defaultData from "../logic/defaultData.js";
 
 export default function Board() {
     const [currentScore, setCurrentScore] = useState(0)
     const [bestScore, setBestScore] = useState(0);
     const [shuffle, setShuffle] = useState(false);
+    const [flip, setFlip] = useState(false);
+
     const [clickedCard, setClickedCard] = useState([]);
-    const [deck, setDeck] = useState(null);
+    const [deck, setDeck] = useState(defaultData);
     const [gameFinished, setGameFinished] = useState(false);
     const getDeck = `https://deckofcardsapi.com/api/deck/new/draw/?count=10`;
  
@@ -28,7 +31,10 @@ export default function Board() {
     useEffect(() => {
         if(clickedCard.length % 5 === 0) {
             fetch(getDeck)
-                .then(response => response.json())
+                .then(response => {
+                    console.log(response)
+                    return response.json()      
+                })
                 .then(data => setDeck(data));
         }
     }, [clickedCard, getDeck])
@@ -42,8 +48,12 @@ export default function Board() {
         setShuffle(false);
     }
 
+    if (flip) {
+        setTimeout(() => setFlip(false), 1000)
+    }
+
     function cardClickHandler(e) {
-        checkGameStatus(setGameFinished, clickedCard, e.target.id)
+        setFlip(true);
         manageScore(
             clickedCard, 
             e.target.id,
@@ -52,6 +62,7 @@ export default function Board() {
             bestScore,
             setBestScore
         );
+        checkGameStatus(setGameFinished, clickedCard, e.target.id)
         setClickedCard([...clickedCard, e.target.id])
         return setShuffle(true);
     }
@@ -97,6 +108,7 @@ function Cards({deck, cardClickHandler}) {
             {deck.cards.map((card) => {
                 return (
                     <Card 
+                        success={deck.success}
                         card={card.image}
                         value={card.value}
                         suit={card.suit}
